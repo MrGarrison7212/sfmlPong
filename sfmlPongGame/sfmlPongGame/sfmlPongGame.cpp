1291534f3dda5a79f6dc96e4855f159c1e0aea9e
MrGarrison7212 <bokipfc1992@gmail.com>
2022-01-13 03:46:31 +0100

    Adding background music + additional sound effects.

--- sfmlPongGame/sfmlPongGame/sfmlPongGame.cpp
+++ sfmlPongGame/sfmlPongGame/sfmlPongGame.cpp
@@ -3,8 +3,10 @@
 
 #include "pch.h"
 #include <iostream>
+#include <sstream>
 #include <SFML\Graphics.hpp>
 #include <SFML\Audio.hpp>
+
 
 int main()
 {
@@ -26,6 +28,14 @@ int main()
 		return 1;
 	}
 
+	// score
+	sf::Text score;
+	score.setFont(font);
+	score.setCharacterSize(30);
+	score.setFillColor(sf::Color::Yellow);
+	score.setPosition(370, 10);
+	score.setString("0 : 0");
+
 	//images
 	sf::Texture tex_pad;
 	sf::Texture tex_ball;
@@ -46,13 +56,41 @@ int main()
 	//sounds
 
 	sf::SoundBuffer buff_hit;
+	sf::SoundBuffer goalP1;
+	sf::SoundBuffer goalP2;
+
 
 	sf::Sound hit;
+	sf::Sound pad1goal;
+	sf::Sound pad2goal;
+
+	sf::Music music;
+
+
 	hit.setBuffer(buff_hit);
+	pad1goal.setBuffer(goalP1);
+	pad2goal.setBuffer(goalP2);
+
+
+	if (goalP1.loadFromFile("Data/goal1.wav") == false) {
+		return -1;
+	}
+
+	if (goalP2.loadFromFile("Data/goal2.wav") == false) {
+		return -1;
+	}
 
 	if (buff_hit.loadFromFile("Data/hit.wav") == false) {
 		return -1;
 	}
+
+	if (music.openFromFile("Data/beat.ogg") == false) {
+		return -1;
+	}
+
+
+	music.play();
+	music.setLoop(true);
 
 	//States
 	bool up = false;
@@ -63,6 +101,8 @@ int main()
 	int xVelocityBall = -4;
 	int yVelocityBall = -4;
 	int yVelocityPad2 = 0;
+	int pad1Score = 0;
+	int pad2Score = 0;
 
 
 	//Shapes
@@ -75,14 +115,14 @@ int main()
 
 	//pad1
 	sf::RectangleShape pad1;
-	pad1.setSize(sf::Vector2f(30, 150));
+	pad1.setSize(sf::Vector2f(20, 100));
 	pad1.setPosition(50, 200);
 	pad1.setTexture(&tex_pad);
 
 	//pad2
 	sf::RectangleShape pad2;
-	pad2.setSize(sf::Vector2f(30, 150));
-	pad2.setPosition(720, 200);
+	pad2.setSize(sf::Vector2f(20, 100));
+	pad2.setPosition(700, 200);
 	pad2.setTexture(&tex_pad);
 
 	//ball
@@ -93,6 +133,7 @@ int main()
 
 	//game loop
 	while (play == true) {
+
 
 		//Events
 		while (window.pollEvent(event)) {
@@ -150,11 +191,11 @@ int main()
 
 		//pad2- AI
 		if (ball.getPosition().y < pad2.getPosition().y) {
-			yVelocityPad2 = -2;
+			yVelocityPad2 = -3;
 		}
 
 		if (ball.getPosition().y > pad2.getPosition().y) {
-			yVelocityPad2 = 2;
+			yVelocityPad2 = 3;
 		}
 
 		pad2.move(0, yVelocityPad2);
@@ -170,6 +211,19 @@ int main()
 
 		if (ball.getPosition().y > 570) {
 			yVelocityBall = -yVelocityBall;
+		}
+
+		//score out of bounds
+		if (ball.getPosition().x < -50) {
+			pad2Score++;
+			pad2goal.play();
+			ball.setPosition(300, 200);
+		}
+
+		if (ball.getPosition().x > 800) {
+			pad1Score++;
+			pad1goal.play();
+			ball.setPosition(300, 200);
 		}
 
 		//collision with pad1
@@ -189,6 +243,11 @@ int main()
 		window.draw(pad1);
 		window.draw(pad2);
 		window.draw(ball);
+		//Score
+		std::stringstream text;
+		text << pad1Score << " : " << pad2Score;
+		score.setString(text.str());
+		window.draw(score);
 
 
 		window.display();
